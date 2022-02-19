b209b51fcde930c31d236a22925d31010271eab2
shuaiwei <weishuai0716@yahoo.com>
2022-02-19 15:32:41 -0500

    Update dbconnect.inc.php

--- javaMysqlWs/submit/dbconnect.inc.php
+++ javaMysqlWs/submit/dbconnect.inc.php
@@ -1,10 +1,8 @@
 <?php
  /* connect information */
- $user = "wei6";
- $password = "wsx891016";
- $database = "wei6_201401_cpsc862";
- $host = "mysql1.cs.clemson.edu";
- $FileDestination = "Applications.txt";
+ $user = "*";
+ $password = "*";
+ $database = "*";
+ $host = "*";
+ $FileDestination = "data";
 ?>
-
-
