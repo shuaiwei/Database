26daa42710ca1d24d78e39100cb3120c013f9c59
shuaiwei <weishuai0716@yahoo.com>
2022-02-19 15:29:35 -0500

    Update dbconnect.inc.php

--- dbconnect.inc.php
+++ dbconnect.inc.php
@@ -1,9 +1,9 @@
 <?php
  /* connect information */
- $user = "wei6";
- $password = "wsx891016";
- $database = "wei6_201401_cpsc862";
- $host = "mysql1.cs.clemson.edu";
+ $user = "*";
+ $password = "*";
+ $database = "*";
+ $host = "*";
  $FileDestination = "data";
 ?>
 
